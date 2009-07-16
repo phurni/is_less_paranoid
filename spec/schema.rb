f7fcea52bf3ca388e5c919aec3cc9513176479f8
Pascal Hurni <pascal_hurni@fastmail.fm>
2009-07-17 00:25:54 +0200

    Added specs for is_less_paranoid

--- spec/schema.rb
+++ spec/schema.rb
@@ -48,4 +48,40 @@ ActiveRecord::Schema.define(:version => 20090317164830) do
     t.string   "name"
     t.boolean  "alive", :default => true
   end
+
+  create_table "companies", :force => true do |t|
+    t.string   "name"
+    t.datetime "deleted_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "contacts", :force => true do |t|
+    t.string   "firstname"
+    t.string   "lastname"
+    t.integer "company_id"
+    t.string "type"
+    t.datetime "deleted_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "addresses", :force => true do |t|
+    t.string   "street"
+    t.string   "city"
+    t.integer   "zipcode"
+    t.integer "contact_id"
+    t.datetime "deleted_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.integer "company_id"
+    t.integer "manager_id"
+    t.datetime "deleted_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 end
